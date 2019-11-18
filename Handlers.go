9def2bb398ef32f6ec38a1f2b8b3a5f08dbdede3
Jack Cloudman <maylo360xd@gmail.com>
2019-11-17 22:00:26 -0600

    add files to db

---
+++ Handlers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string  `json:"message"`
+	Files   []*File `json:"files,omitempty"`
+	Error   string  `json:"error,omitempty"`
+	Users   []*Peer `json:"users,omitempty"`
+}
+
+func writeResponse(w http.ResponseWriter, message Response, status int) {
+	w.WriteHeader(status)
+	response, _ := json.Marshal(message)
+	w.Write(response)
+}
+func searchByHashHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	r.ParseForm()              // Parses the request body
+	hash := r.Form.Get("hash") // x will be "" if parameter is not set
+	m := Response{}
+	if hash == "" {
+		m.Message = "Falta hash para hacer la busqueda"
+		writeResponse(w, m, http.StatusBadRequest)
+		return
+	}
+	f := searchByHash(hash)
+	if f == nil {
+		m.Message = "Archivo no encontrado"
+		writeResponse(w, m, http.StatusNotFound)
+		return
+	}
+	m.Files = append(m.Files, f)
+	writeResponse(w, m, http.StatusOK)
+}
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	request := &Response{}
+	err := json.NewDecoder(r.Body).Decode(request)
+	if err != nil {
+		response := &Response{}
+		response.Message = "Error al procesar la solicitud"
+		response.Error = err.Error()
+		writeResponse(w, *response, http.StatusBadRequest)
+		log.Print(err.Error())
+		return
+	}
+	if request.Files == nil || request.Users == nil {
+		response := &Response{}
+		response.Message = "Error al procesar la solicitud"
+		writeResponse(w, *response, http.StatusBadRequest)
+		return
+	}
+	uploadfile(request.Files[0], request.Users[0])
+	fmt.Printf("%+v\n", request.Files[0])
+	fmt.Printf("%+v\n", request.Users[0])
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/getInfo/", searchByHashHandler).Methods("POST")
+	r.HandleFunc("/uploadFile/", uploadFileHandler).Methods("POST")
+	startSql()
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println("Escuchando...")
+	log.Fatal(srv.ListenAndServe())
+}
